afbfcd76868499afce363638e069809422bcaa76
Tomohiro Hosaka <bokutin@bokut.in>
2011-08-21 23:03:07 +0900

    add Makefile.PL

--- lib/Mason/Plugin/UTF8.pm
+++ lib/Mason/Plugin/UTF8.pm
@@ -4,3 +4,14 @@ use Moose;
 with 'Mason::Plugin';
 
 1;
+
+=head1 AUTHOR
+
+Tomohiro Hosaka E<lt>bokutin@bokut.inE<gt>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2011 Tomohiro Hosaka.  All Rights Reserved.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
